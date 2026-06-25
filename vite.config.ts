fc1b102234192b97ba90010ba4126c518fecb066
rhari44 <rhari44@gmail.com>
2026-06-25 10:05:09 +0300

    Enhance Vite config with esbuild and build options
    Added esbuild configuration to suppress warnings and specified rollup options for the build process.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  },
+  build: {
+    rollupOptions: {}
+  }
 })
